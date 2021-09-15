d86a3b4dbd208b0542951ea3fd4fcd30e0561f8e
kamilpacz1 <paczynski.kamil1@gmail.com>
2021-09-15 14:39:17 +0200

    Initial commit

---
+++ src/main/java/project_hello_spring/lang/Lang.java
@@ -0,0 +1,54 @@
+package project_hello_spring.lang;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "languages")
+public class Lang {
+    @Id
+    @GeneratedValue(generator = "inc")
+    @GenericGenerator(name = "inc", strategy = "increment")
+    private Integer id;
+    @Column(name = "welcomemsg")
+    private String welcomeMsg;
+    private String code;
+
+    /**
+     * Hibernate (JPA) needs it.
+     */
+    @SuppressWarnings("unused")
+    Lang() {
+    }
+
+    public Lang(Integer id, String welcomeMsg, String code) {
+        this.id = id;
+        this.welcomeMsg = welcomeMsg;
+        this.code = code;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getWelcomeMsg() {
+        return welcomeMsg;
+    }
+
+    public void setWelcomeMsg(String welcomeMsg) {
+        this.welcomeMsg = welcomeMsg;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
